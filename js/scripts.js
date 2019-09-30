2416205ec654708a66dca67bb53506d16172a6e4
Ian722 <iwarui722@gmail.com>
2019-09-30 09:02:46 +0300

    add social image files

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+ 
+//  $(document).ready(function(){
+//          $(".submit").click(fuction(){
+//                    event.preventDefault();
+//                    console.log("Clicked button");
+        
+//                    var email = $(".email").val();
+//                    var name = $(".name").val();
+//                    var message = $(".message").val();
+//                    Var statusElm = $(".status");
+        
+//                    if(email.length > 5 && email.include("@") && email.include("."){
+//                        console.log("Email is valid");
+//                    });
+//                     else {
+//                       console.log("Email is not valid");
+//                   }
+//                });
+//        })
+    
+         
+//  $(document).ready(function(){
+//          $(".submit").click(fuction(){
+//                    event.preventDefault();
+//                    console.log("Clicked button");
+        
+//                    var email = $(".email").val();
+//                    var name = $(".name").val();
+//                    var message = $(".message").val();
+//                    Var statusElm = $(".status");
+        
+//                    if(email.length > 5 && email.include("@") && email.include("."){
+//                        console.log("Email is valid");
+//                    });
+//                     else {
+//                       console.log("Email is not valid");
+//                   }
+//                });
+//        })
+    
+        
